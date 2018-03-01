e4dadf43f80389ff4bc7058bd948210afb15b88d
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-03-01 21:18:23 +0100

    New begining

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,51 +5,18 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { UserModule } from './user/user.module';
-
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-
-import { MenuComponent } from './menu/menu.component';
-import { HamburgerComponent } from './hamburger/hamburger.component';
-
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
-import { CoverComponent } from './cover/cover.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginModule } from './login/login.module';
-import { AuthModule } from './auth/auth.module';
-import { TourOfHeroesModule } from './tour-of-heroes/tour-of-heroes.module';
-import { UnlessDirective } from './unless/unless.directive';
-import { UnlessDirectiveTestComponent } from './unless-directive-test/unless-directive-test.component';
-import { ClickableDirective } from './clickable/clickable.directive';
-import { ChildComponent } from './child/child.component';
-import { SizerComponent } from './sizer/sizer.component';
-import { SizerNumberComponent } from './sizer-number/sizer-number.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MenuComponent,
-    HamburgerComponent,
-    CoverComponent,
-    DashboardComponent,
-    UnlessDirective,
-    UnlessDirectiveTestComponent,
-    ClickableDirective,
-    ChildComponent,
-    SizerComponent,
-    SizerNumberComponent
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFontAwesomeModule,
     BrowserAnimationsModule,
-    UserModule,
-    LoginModule,
-    AuthModule,
-    TourOfHeroesModule,
     FormsModule
   ],
   providers: [],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/map';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  name = '';
-  fontSize1 = 10;
-  fontSize2 = 10;
-  fontSize3 = 10;
 
   constructor() { }
 
   ngOnInit() {
   }
+
 }
