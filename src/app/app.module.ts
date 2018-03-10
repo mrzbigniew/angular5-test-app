9897c44a2ec028a2269895244ceb4748c5939822
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-03-11 00:25:18 +0100

    Added imports for navigation guard user confirm dialog toast message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { NavigationGuardModule } from './navigation-guard/navigation-guard.module';
+import { UserConfirmationDialogModule } from './user-confirmation-dialog/user-confirmation-dialog.module';
+import { ToastMessageModule } from './toast-message/toast-message.module';
 import { HeaderModule } from './header/header.module';
 
 import { AppComponent } from './app.component';
@@ -19,8 +21,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    HeaderModule
+    HeaderModule,
+    NavigationGuardModule,
+    UserConfirmationDialogModule,
+    ToastMessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
