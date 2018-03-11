15088f50c2e36b9374dfb87d4095b826856898c4
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2018-03-11 18:03:02 +0100

    Renamed navigation guard to transition guard

--- src/app/navigation-guard/guard-registration-params.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs/Observable';
-
-export interface GuardRegistrationParams {
-    dataChangesObserver: Observable<boolean>;
-    useCancelSaveDiscardModal: boolean;
-    onSave: () => Observable<boolean>;
-    onDiscard?: () => Observable<boolean>;
-    onCancel?: () => Observable<boolean>;
-}
